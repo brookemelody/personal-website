3d2087d4ca00693783e32a52de73a3cff0203751
Brooke <brookewu2023@gmail.com>
2024-12-19 10:49:53 -0500

    Update homepage info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         <div className="flex flex-col gap-3 md:text-lg sm:text-sm">
           <h2 className="md:text-3xl sm:text-xl animate-loadFromLeft"><u className="text-pink-500 dark:text-pink-400"><b className="text-pink-500 dark:text-pink-400 font-serif">Hello World!</b></u></h2>
           <p className="animate-loadFromLeft">{`My name is Brooke, and I'm currently a sophomore college student studying Computer Science at NC State University's College of Engineering.`}</p>
-          <p className="animate-loadFromLeft">This is my personal website, coded by me using the <OutgoingLink linkText='Next.js' link='https://nextjs.org/'/> web framework with <OutgoingLink linkText="Tailwind CSS" link="https://tailwindcss.com/"/> and <OutgoingLink linkText="TypeScript" link="https://www.typescriptlang.org/"/> (and lots of flexboxes!)</p>
+          <p className="animate-loadFromLeft">This is my personal website, coded by me using the <OutgoingLink linkText='Next.js' link='https://nextjs.org/'/> web framework for <OutgoingLink linkText="React.js" link="https://react.dev/"/>, with <OutgoingLink linkText="Tailwind CSS" link="https://tailwindcss.com/"/> and <OutgoingLink linkText="TypeScript" link="https://www.typescriptlang.org/"/> (and lots of flexboxes!). It is hosted on <OutgoingLink linkText="Vercel" link="https://vercel.com/"/>.</p>
           <div className="animate-loadFromLeft">
             <NavButton buttonName="resume" link="/resume"/>
           </div>
